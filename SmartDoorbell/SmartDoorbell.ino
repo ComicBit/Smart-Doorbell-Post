09a9a64c79f5624c18e0e78b5ffbc3ca064de432
Leandro Piccione 🌋 <comicbit@gmail.com>
2023-07-31 13:05:26 +0200

    Update SmartDoorbell code with new network credentials and add functionality for sleep mode, POST counter, and serial input commands.
    - Add sleepModeEnabled variable to enable/disable sleep mode
    - Add postCount variable to keep track of POST calls
    - Implement serial input commands: "netstats", "pinstate", "postcount", "sleepmode y", "sleepmode n", and "ring"
    - Print network status and IP address with the command "netstats"
    - Print doorbell pin state with the command "pinstate"

--- SmartDoorbell/SmartDoorbell.ino
+++ SmartDoorbell/SmartDoorbell.ino
@@ -9,8 +9,11 @@ const char* ssid = "<Your Network SSID>";
 const char* password = "<Your Network Password>";
 
 volatile bool doorbellRang = false;
+bool sleepModeEnabled = false; // Set to false to disable sleep mode
+int postCount = 0; // Counter for POST calls
 
 void setup() {
+  Serial.begin(115200);
   pinMode(DOORBELL_PIN, INPUT_PULLUP);
   attachInterrupt(digitalPinToInterrupt(DOORBELL_PIN), doorbellRing, FALLING);
 
@@ -21,12 +24,37 @@ void loop() {
   if (doorbellRang) {
     doorbellRang = false; // Reset the flag
     sendNotification();
+    postCount++; // Increase POST counter
     WiFi.disconnect();
     delay(COOLDOWN_TIME); // Cooldown period
   }
 
-  esp_sleep_enable_ext0_wakeup(GPIO_NUM_4, LOW); //1 = High, 0 = Low
-  esp_deep_sleep_start();
+  // Check Serial input
+  while (Serial.available()) {
+    String inputString = Serial.readStringUntil('\n');
+
+    if (inputString == "netstats") {
+      printNetworkStatus();
+    } else if (inputString == "pinstate") {
+      printPinState();
+    } else if (inputString == "postcount") {
+      Serial.println("HTTP POST has been called " + String(postCount) + " times.");
+    } else if (inputString == "sleepmode y") {
+      sleepModeEnabled = true;
+      Serial.println("Sleep mode enabled");
+    } else if (inputString == "sleepmode n") {
+      sleepModeEnabled = false;
+      Serial.println("Sleep mode disabled");
+    } else if (inputString == "ring") {
+      doorbellRang = true;
+      Serial.println("Doorbell ring simulated");
+    }
+  }
+
+  if (sleepModeEnabled) {
+    esp_sleep_enable_ext0_wakeup(GPIO_NUM_4, LOW); //1 = High, 0 = Low
+    esp_deep_sleep_start();
+  }
 }
 
 void doorbellRing() {
@@ -41,16 +69,32 @@ void doorbellRing() {
 }
 
 void connectToWiFi() {
-  WiFi.begin(ssid, password);
+  if (WiFi.status() != WL_CONNECTED) {
+    WiFi.begin(ssid, password);
 
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(1000);
     Serial.println("Connecting to WiFi...");
+    unsigned long startTime = millis();
+
+    // Try to connect for up to 30 seconds
+    while (WiFi.status() != WL_CONNECTED && millis() - startTime < 30000) {
+      delay(500);
+      Serial.print(".");
+    }
+
+    // If we couldn't connect within 30 seconds, print an error message
+    if (WiFi.status() != WL_CONNECTED) {
+      Serial.println("Failed to connect to WiFi");
+    } else {
+      Serial.println("Connected to WiFi");
+      Serial.print("IP address: ");
+      Serial.println(WiFi.localIP().toString());
+    }
   }
-  Serial.println("Connected to WiFi");
 }
 
 void sendNotification() {
+  connectToWiFi(); // Check the WiFi connection
+
   if ((WiFi.status() == WL_CONNECTED)) {
     HTTPClient http;
 
@@ -68,5 +112,23 @@ void sendNotification() {
   }
   else {
     Serial.println("Error in WiFi connection");
+  }
+}
+
+void printNetworkStatus() {
+  if (WiFi.status() == WL_CONNECTED) {
+    Serial.println("Connected to WiFi network");
+    Serial.print("IP address: ");
+    Serial.println(WiFi.localIP().toString());
+  } else {
+    Serial.println("Not connected to WiFi network");
+  }
+}
+
+void printPinState() {
+  if (digitalRead(DOORBELL_PIN) == HIGH) {
+    Serial.println("Doorbell pin is HIGH");
+  } else {
+    Serial.println("Doorbell pin is LOW");
   }
 }
